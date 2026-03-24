5ebc9fcbc0001f7cf5b475a288ea89c4f4037a7a
Kyunghwan Choi <stressoutcompany@gmail.com>
2026-03-24 18:10:25 +0900

    블로그 글 추가

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -11,12 +11,9 @@ export function generateStaticParams() {
   return posts.map((post) => ({ slug: post.slug }));
 }
 
-interface PageProps {
-  params: { slug: string };
-}
-
-export default function BlogPostPage({ params }: PageProps) {
-  const post = getPostBySlug(params.slug);
+export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const post = getPostBySlug(slug);
 
   if (!post) {
     notFound();
